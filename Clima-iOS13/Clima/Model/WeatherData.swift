24acda495fdffca5c9fac6d68b3b9965e239740f
Barış Kandemir <48553738+bariskndmr@users.noreply.github.com>
2022-10-26 14:51:01 +0300

    learned how to use computed property and cleaned code

--- Clima-iOS13/Clima/Model/WeatherData.swift
+++ Clima-iOS13/Clima/Model/WeatherData.swift
@@ -9,15 +9,17 @@
 import Foundation
 
 struct WeatherData: Decodable {
-  let name: String
-  let main: Main
-  let weather: [Weather]
+    let name: String
+    let main: Main
+    let weather: [Weather]
 }
 
 struct Main: Decodable {
-  let temp: Double
+    let temp: Double
 }
 
 struct Weather: Decodable{
-  let description: String
+    let description: String
+    let id: Int
+    
 }
--- Clima-iOS13/Clima/Model/WeatherManager.swift
+++ Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -9,47 +9,53 @@
 import Foundation
 
 struct WeatherManager {
-  let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=828966dd00e73ffad60098054ebbbb37&units=metric"
-  
-  func fetchWeather(cityName: String){
-    let urlString = "\(weatherURL)&q=\(cityName)"
-    performRequest(urlString: urlString)
-  }
-  
-  func performRequest(urlString: String) {
-    // 1. Create URL
-    if let url = URL(string: urlString){
-      // 2. Create a URLSession
-      let session = URLSession(configuration: .default)
-      
-      // 3. Give the session task
-      let task = session.dataTask(with: url) { (data, response, error) in
-        // if we were use "if let" in here we couldn't return
-        if error != nil {
-          print(error!)
-          return
-        }
-        if let safeData = data {
-          parseJSON(weatherData: safeData)
-          
-        }
-      }
-      
-      // 4. Start the task
-      task.resume()
-    }
-  }
-  
-  func parseJSON(weatherData: Data){
-    let decoder = JSONDecoder()
-    do{
-      let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-      print(decodedData.weather[0].description)
-    } catch{
-      print(error)
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=828966dd00e73ffad60098054ebbbb37&units=metric"
+    
+    func fetchWeather(cityName: String){
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
     }
     
-  }
-  
-  
+    
+    func performRequest(urlString: String) {
+        // 1. Create URL
+        if let url = URL(string: urlString){
+            // 2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            // 3. Give the session task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                // if we were use "if let" in here we couldn't return
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                }
+            }
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    
+    
+    func parseJSON(weatherData: Data){
+        let decoder = JSONDecoder()
+        do{
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.conditionName)
+        } catch{
+            print(error)
+        }
+        
+    }
+    
+    
 }
