2df3fec690f8ba203ddff6d35093e68d165bc973
bariskndmr <bariskandemirx@gmail.com>
2022-11-27 14:21:38 +0300

    user can be login with firebase authentication

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
-
-    @IBOutlet weak var emailTextfield: UITextField!
-    @IBOutlet weak var passwordTextfield: UITextField!
+  
+  @IBOutlet weak var emailTextfield: UITextField!
+  @IBOutlet weak var passwordTextfield: UITextField!
+  
+  
+  @IBAction func loginPressed(_ sender: UIButton) {
     
-
-    @IBAction func loginPressed(_ sender: UIButton) {
+    if let email = emailTextfield.text, let password = passwordTextfield.text{
+      Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+        if let err = error {
+          print("Sign In Error : \(err.localizedDescription)")
+        } else {
+          self.performSegue(withIdentifier: "LoginToChat", sender: self)
+        }
+      }
     }
-    
+  }
+  
 }
