ba49c22a515391c61869e45dd3cf1a8707047715
bariskndmr <bariskandemirx@gmail.com>
2022-11-27 13:57:14 +0300

    user can be register with firebase authtentication

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
-    @IBOutlet weak var emailTextfield: UITextField!
-    @IBOutlet weak var passwordTextfield: UITextField!
-    
-    @IBAction func registerPressed(_ sender: UIButton) {
+  
+  @IBOutlet weak var emailTextfield: UITextField!
+  @IBOutlet weak var passwordTextfield: UITextField!
+  
+  @IBAction func registerPressed(_ sender: UIButton) {
+    if let email = emailTextfield.text, let password = passwordTextfield.text {
+      Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+        if let err = error {
+          print("Register error : \(err.localizedDescription)")
+        } else {
+          self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+        }
+        
+      }
     }
-    
+  }
+  
 }
